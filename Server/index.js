59143954396dfc9b76ca61a2c76b3b8663ba65b7
vishal vishwas <vishalvishwas7082@gmail.com>
2025-04-30 11:52:21 +0530

    server

--- Server/index.js
+++ Server/index.js
@@ -1,37 +1,43 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const EmployeeModel = require('./models/Employee')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const EmployeeModel = require('./models/Employee');
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/employee")
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("MongoDB connection error:", err));
 
-app.post('/login', (req, res)=>{
-    const {email, password} = req.body;
-    EmployeeModel.findOne({email:email})
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  EmployeeModel.findOne({ email: email })
     .then(user => {
-        if(user){
-            if(user.password === password){
-                res.json("success")
-            } else {
-                res.json("password is incorrect")
-            }
+      if (user) {
+        if (user.password === password) {
+          res.json("success");
         } else {
-            res.json("no record exist")
+          res.json("password is incorrect");
         }
+      } else {
+        res.json("no record exist");
+      }
     })
-})
+    .catch(err => res.json(err));
+});
 
+app.post('/register', (req, res) => {
+  EmployeeModel.create(req.body)
+    .then(employee => res.json(employee))
+    .catch(err => res.json(err));
+});
 
-app.post('/register', (req, res)=>{
-    EmployeeModel.create(req.body)
-    .then(employee => res.json(employees))
-    .catch(err => res.json(err))
-})
-
-app.listen(3001, ()=> {
-    console.log("server is running")
-})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
